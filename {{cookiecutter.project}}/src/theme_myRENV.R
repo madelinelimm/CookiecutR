f1674f341e5acd990426d68d92ba7b3e3755f920
madelinelimm <71245661+madelinelimm@users.noreply.github.com>
2022-01-17 00:32:02 +0800

    created new ggplot theme

--- {{cookiecutter.project}}/src/theme_myRENV.R
+++ {{cookiecutter.project}}/src/theme_myRENV.R
@@ -2,28 +2,32 @@
 
 theme_myRENV <- function(){
   theme_light() %+replace%
-  theme(
+    theme(
       
-  line = element_line(colour = 'grey30', size = 1),
-  text = element_text(colour = "lightpink4", face = 'bold' , size = 15 ),
+      line = element_line(colour = 'grey30', size = 1),
+      text = element_text(colour = "lightpink4", face = 'bold' , size = 15 ),
       
-  plot.title = element_text(size = 22, family = '' , 
-                            face = 'bold', hjust = 0.5, 
-                            vjust = 1.5, colour = 'blue4'),
-  
-  axis.ticks = element_line(),
+      plot.title = element_text(size = 18, family = '' , 
+                                face = 'bold', hjust = 0.5, 
+                                vjust = 1.5, colour = 'blue4'),
+      
+      axis.ticks = element_line(),
       
       
-  panel.background = element_rect(fill = "white"),
-  plot.margin = margin(2, 1, 1, 1, "cm"),
-  plot.background = element_rect(fill = "papayawhip"),
+      panel.background = element_rect(fill = "white"),
+      plot.margin = margin(2, 1, 1, 1, "cm"),
+      plot.background = element_rect(fill = "papayawhip"),
       
-  legend.key = element_blank(),
-  legend.position = 'right',
+      legend.key = element_blank(),
+      legend.position = 'right',
+      legend.key.size = unit(0.5, 'cm'), #change legend key size
+      #legend.key.height = unit(1, 'cm'), #change legend key height
+      #legend.key.width = unit(1, 'cm'), #change legend key width
+      #legend.title = element_text(size=14), #change legend title font size
+      legend.text = element_text(size=10) #change legend text font size
       
-  )
+    )
 }
-
 
 
 
