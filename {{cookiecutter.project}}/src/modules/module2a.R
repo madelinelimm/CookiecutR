3039748f0ff2907d0985c341885e658367f0da84
madelinelimm <71245661+madelinelimm@users.noreply.github.com>
2022-01-17 00:30:50 +0800

    new module using ggplot theme

--- {{cookiecutter.project}}/src/modules/module2a.R
+++ {{cookiecutter.project}}/src/modules/module2a.R
@@ -6,31 +6,7 @@ cat("\n========================================================\n")
 
 library(ggplot2)
 
-#source('src/theme_myRENV.R')
-
-theme_myRENV <- function(){
-  theme_light() %+replace%
-    theme(
-      
-      line = element_line(colour = 'grey30', size = 1),
-      text = element_text(colour = "lightpink4", face = 'bold' , size = 15 ),
-      
-      plot.title = element_text(size = 22, family = '' , 
-                                face = 'bold', hjust = 0.5, 
-                                vjust = 1.5, colour = 'blue4'),
-      
-      axis.ticks = element_line(),
-      
-      
-      panel.background = element_rect(fill = "white"),
-      plot.margin = margin(2, 1, 1, 1, "cm"),
-      plot.background = element_rect(fill = "papayawhip"),
-      
-      legend.key = element_blank(),
-      legend.position = 'right',
-      
-    )
-}
+source('src/theme_myRENV.R')
 
 
 ggplot(data = cars, aes(speed, dist))+
