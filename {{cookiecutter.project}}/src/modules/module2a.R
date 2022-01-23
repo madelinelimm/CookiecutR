a94d79a42846c58170b2a4c065fd7cf7240a9a86
madelinelimm <71245661+madelinelimm@users.noreply.github.com>
2022-01-23 16:28:52 +0800

    additional module with ggplot plots

--- {{cookiecutter.project}}/src/modules/module2a.R
+++ {{cookiecutter.project}}/src/modules/module2a.R
@@ -4,14 +4,15 @@ cat("\n========================================================\n")
 print('Entering Module 2a')
 cat("\n========================================================\n")
 
-
+pdf("rplot_speed_vs_dist.pdf") 
 ggplot(data = cars, aes(speed, dist))+
   geom_point() +
   ggtitle('Plot of speed vs distance') 
+dev.off() 
 
-
+pdf("rplot_hwy_vs_displ.pdf") 
 ggplot(mpg, aes(displ, hwy, colour = class))  + geom_point() +
   ggtitle('Plot of highway miles vs engine displacement') 
-
+dev.off() 
 
 cat("\n--------------------------------------------------------------\n")
