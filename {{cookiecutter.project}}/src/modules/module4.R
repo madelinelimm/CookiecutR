d1836879ebd416a35347134a06c8205a87da3f81
Madeline Lim <madeline.lim833@gmail.com>
2022-02-13 16:03:36 +0800

    sample module code connecting to PostgreSQL database

---
+++ {{cookiecutter.project}}/src/modules/module4.R
@@ -0,0 +1,26 @@
+## Sample code to connect to database with R
+
+cat("\n========================================================\n")
+print('Entering Module 4')
+cat("\n========================================================\n")
+
+library(RPostgres)
+library(DBI)
+library(rjson)
+
+db<- fromJSON(file = 'config/db.template.json')
+dbcon <- db$cdm$connection
+
+connect <- dbConnect(RPostgres::Postgres(),
+                 dbname = dbcon$dbname,
+                 host = dbcon$host,
+                 port = dbcon$port,
+                 password = dbcon$password,
+                 user = dbcon$user)
+
+table <- (dbGetQuery(connect,
+           "SELECT * FROM testing_schema.accounts"))
+
+print(table)
+
+cat("\n--------------------------------------------------------------\n")
