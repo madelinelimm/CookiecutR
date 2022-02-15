c53f192977bceadd031127a550a3ede2d38983ab
Madeline Lim <madeline.lim833@gmail.com>
2022-02-15 09:43:29 +0800

    consist of git commands

--- {{cookiecutter.project}}/bin/MakeGit.sh
+++ {{cookiecutter.project}}/bin/MakeGit.sh
@@ -3,7 +3,7 @@
     git add .; true
     git remote add origin {{cookiecutter.remote}}
     git commit -m 'This is the initial commit'
-    # Geenerate the master branch
+    # Generate the master branch
     # ......................................
     git push --set-upstream origin master
 
