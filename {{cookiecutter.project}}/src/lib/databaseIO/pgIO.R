152c464d5b07434f9713d24abdac11eb3d6e60e9
Madeline Lim <madeline.lim833@gmail.com>
2022-02-17 18:38:17 +0800

    function getAllData that outputs table from database when given query

--- {{cookiecutter.project}}/src/lib/databaseIO/pgIO.R
+++ {{cookiecutter.project}}/src/lib/databaseIO/pgIO.R
@@ -6,11 +6,17 @@ library(rjson)
 
 
 # subfunction
-connectToDB <- function(query){
+connectToDB <- function(query, dbname = NULL){
   db <- fromJSON(file = 'config/db.template.json')
   dbcon <- db$cdm$connection
   
-  dbname_ <- dbcon$dbname
+  if (is.null(dbname)){
+    dbname_ <- dbcon$dbname
+  }
+  else{
+    dbname_ <- dbname
+  }
+  
   if (is.null(dbname_) & !is.null(db$defaultDB)){
     dbname_ <- db$defaultDB
   }
@@ -29,8 +35,13 @@ connectToDB <- function(query){
 }
 
 # main function
-getAllData <- function(query){
-  tryCatch(connectToDB(query),
+# takes in query and dbname(optional)
+# if dbname is stated, then it will be the database used for this query
+# else the dbname in db.template.json will be used
+# outputs table 
+getAllData <- function(query, dbname = NULL){
+
+  tryCatch(connectToDB(query, dbname),
             error = function(e)
               if (length(e) > 0){
                 print(e)
