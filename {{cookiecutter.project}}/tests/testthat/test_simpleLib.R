f4b6732933c898022199d7a0d6d8f5a62b8b3d49
Madeline Lim <madeline.lim833@gmail.com>
2022-02-24 13:39:54 +0800

    for testing of functions

--- {{cookiecutter.project}}/tests/testthat/test_simpleLib.R
+++ {{cookiecutter.project}}/tests/testthat/test_simpleLib.R
@@ -1,10 +1,13 @@
-# test the function in src/lib/testLib/simpleLin.R
+# test the function in src/lib/testLib/simpleLib.R
+library(testthat)
 
 test_that('Sum of a and b', {
   source('../../src/lib/testLib/simpleLib.R')
-  expect_error(SimpleTestFunction(1, '1'), "non-numeric argument to binary operator")
-  expect_warning(SimpleTestFunction(c(1,3,4), c(1,2)), 'longer object length is not a multiple of shorter object length')
   
+  expect_error(SimpleTestFunction(1, '1'), "non-numeric argument to binary operator")
+  expect_error(SimpleTestFunction(1, a), "object 'a' not found") 
+  #expect_warning(SimpleTestFunction(c(1,2,3), c(1, 2)), "longer object length is not a multiple of shorter object length")
+
   expect_equal(SimpleTestFunction(1, 2), 3)
   expect_equal(SimpleTestFunction('1', '1'), '11')
   expect_equal(SimpleTestFunction(c(1,3,4), c(2)), c(3, 5, 6))
